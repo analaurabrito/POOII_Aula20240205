560b7a97854bae1e8c4a1e7693179632025daf27
Aura <111578898+analaurabrito@users.noreply.github.com>
2024-05-02 08:22:03 -0300

    Add files via upload

---
+++ Aula20240205Botao/Home.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Aula20240205Botao
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(383, 170);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(13, 13);
+            this.lblContador.TabIndex = 1;
+            this.lblContador.Text = "0";
+            this.lblContador.Click += new System.EventHandler(this.lblContador_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "click down";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.button1);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Aula20240205Botao/Home.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula20240205Botao
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            cont = int.Parse(lblContador.Text);
+            cont++;
+
+            lblContador.Text = cont.ToString();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblContador_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            cont = int.Parse(lblContador.Text);
+            cont--;
+
+            lblContador.Text = cont.ToString();
+        }
+    }
+}
